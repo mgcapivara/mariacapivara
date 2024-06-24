00593dbb1017eb7f4f01fb94ae69ce8a9917220d
mgcapivara <00001081578270sp@al.educacao.sp.gov.br>
2024-06-24 14:24:36 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white");
+    }
+  
+  
+  function draw() {
+    
+    stroke("blue");
+    fill("red");
+    
+    
+    //console.log(mouseIsPressed);
